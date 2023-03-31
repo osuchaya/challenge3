37ba9c17387b688abcdb879bb922513f20bba5f3
Suchaya Osatis <suchaya.osatis@gmail.com>
2023-03-30 23:05:04 -0700

    added the variables and argument conditions

--- script2.js
+++ script2.js
@@ -24,3 +24,43 @@ console.log(upperCases)
 console.log(numberS)
 console.log(specialCharacterS)
 
+function generatePassword (){
+  var outputPassword = ""; //empty placeholder for password to be generated
+
+  var passwordLength; prompt("Select password length between 8 to 128 characters");
+
+  if (passwordLength >=8 && passwordLength <=128);
+  alert("Please choose from the following criteria to include in your password");{
+;
+
+  var options = ""; // placeholder for options selected
+
+  var lowerCases = confirm("Do you want to include lower case letters?");}
+
+  if (lowerCases === true)  {
+    selection = true; options += lowerCases;
+  }
+  
+  var upperCases = confirm("Do you want to include upper case letters?");
+
+  if (upperCases === true) {
+    selection = true; options += upperCases;
+  }
+
+  var numberS = confirm("Do you want to include numbers?");
+  if (numberS === true) {
+    selection = true; options += numberS;
+  }
+  var specialCharacterS = confirm("Do you want to include special characters?");
+  if (specialCharacterS === true) {
+    selection = true; options += specialCharacterS;
+  }
+  
+  else {
+    for (i=0; i < passwordLength; i++){
+      outputPassword = options[Math.floor(Math.random()*options.length)];
+    }};
+  
+  return outputPassword;
+}
+generateBtn.addEventListener("click", writePassword);
